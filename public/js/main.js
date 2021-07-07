37be949ffe4a4a4ae69a151f6e2bca33fbb26f1d
George Atkinson <georgeatkinsonwd@gmail.com>
2021-07-07 17:42:14 +0100

    added delete

---
+++ public/js/main.js
@@ -0,0 +1,27 @@
+const deleteText = document.querySelectorAll('.del')
+
+Array.from(deleteText).forEach((element)=>{
+    element.addEventListener('click',deleteRapper)
+})
+
+async function deleteRapper(){
+    const sName = this.parentNode.childNodes[1].innerText
+    const bName = this.parentNode.childNodes[3].innerText
+    try{
+        const response = await fetch('deleteRapper', {
+            method: 'delete',
+            headers: {'Content-type': 'application/json'},
+            body : JSON.stringify({
+                'stageNameS': sName,
+                'birthNameS': bName
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    }
+    catch(err){
+        console.log(err)
+
+    }
+}
--- server.js
+++ server.js
@@ -2,14 +2,24 @@ const express = require('express')
 const app = express()
 const PORT = 8000
 const MongoClient = require('mongodb').MongoClient
-// const bodyParser = require('body-parser');
-
 
 let db,
-    dbConnectionStr = 'mongodb+srv://george:password@cluster0.7hspw.mongodb.net/rap-list?retryWrites=true&w=majority'
+    dbConnectionStr = 'mongodb+srv://george:wackoacko1994@cluster0.7hspw.mongodb.net/rap-list?retryWrites=true&w=majority'
     dbName = 'rap'
 
-// app.use(bodyParser());
+    
+MongoClient.connect(dbConnectionStr,{
+    useUnifiedTopology: true
+}) .then(client => {
+    console.log(`Connected to ${dbName} Database`)
+    db = client.db(dbName)
+})
+
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 
 
@@ -31,20 +41,18 @@ app.post('/addRapper',(request,response)=>{
     .catch(error => console.error(error))
 })
 
+app.delete('/deleteRapper', (request,response)=>{
+    db.collection('rap-list').deleteOne({stageName: request.body.stageNameS})
+    .then(result =>{
+        console.log('Rapper Deleted')
+        response.json('Rapper Deleted')
+    })
+    .catch(error=>console.error(error))
+})
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
 
 
 app.listen(PORT, function(){
 console.log(`server running on PORT ${PORT}`)
 })
 
-MongoClient.connect(dbConnectionStr,{
-    useUnifiedTopology: true
-}) .then(client => {
-    console.log(`Connected to ${dbName} Database`)
-    db = client.db(dbName)
-})
